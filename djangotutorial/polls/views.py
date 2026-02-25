4da43141c4b3c2d887b12ad619ce285fa1dafbc2
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-02-25 17:02:16 +0100

    mise à jour de la vue detail dans polls/views.py

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse, Http404
 #from django.template import loader
 
 from .models import Question
@@ -14,7 +15,13 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+    return render(request, "polls/detail.html", {"question": question})
+
+    #return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
