433917634a72dc0303d41c6e39fc4cb22c81886c
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-02-27 13:47:10 +0100

    Modification de forms.py pour enrichir le formulaire dans polls/forms.py

--- djangotutorial/polls/forms.py
+++ djangotutorial/polls/forms.py
@@ -4,6 +4,13 @@ from .models import Question
 
 
 class QuestionForm(forms.ModelForm):
+
+    choice1 = forms.CharField(required=False, label="Choix 1")
+    choice2 = forms.CharField(required=False, label="Choix 2")
+    choice3 = forms.CharField(required=False, label="Choix 3")
+    choice4 = forms.CharField(required=False, label="Choix 4")
+    choice5 = forms.CharField(required=False, label="Choix 5")
+
     class Meta:
         model = Question
         fields = ['question_text', 'pub_date']
