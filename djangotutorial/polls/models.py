cbd3c33cce9ae8fc6caa76502543c480e25dee72
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-02 08:24:51 +0100

    Ajout de la méthode def__repr__ dans polls/models.py

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -13,6 +13,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def __repr__(self):
+        return "<Question: {}>".format(self.question_text)
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now 
