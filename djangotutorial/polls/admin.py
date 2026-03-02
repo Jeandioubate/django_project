c6d9fdaa848f4318786ab4928afc215962321e39
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-02 11:52:04 +0100

    suppression de certains imports

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -6,12 +6,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', "was_published_recently")
     list_filter = ('question_text','pub_date')
     ordering = ('question_text', 'pub_date')
-    search_fields = ['question_text', 'pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('question', 'choice_text', 'votes')
